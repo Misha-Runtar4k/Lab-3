d830b6432dc888e0b9675303fddf3d707dc3e61d
Misha-Runtar4k <misabuc81@gmail.com>
2025-09-19 19:00:57 +0300

    Add files via upload

---
+++ lab3 Ktp Buts/lab3 Ktp Buts/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _Lab3_Ktp_Buts
+{
+    class example2
+
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Введіть значення x: ");
+            double.TryParse(Console.ReadLine(), out double x);
+           
+            
+            {
+                double y = Math.Cos(2 * x);
+                double w = Math.Cos(2 * x) - Math.Sin(x);
+
+                Console.WriteLine($"y = cos(2x) = {Math.Round(y, 5)}");
+                Console.WriteLine($"w = cos(2x) - sin(x) = {Math.Round(w, 5)}");
+            }
+             
+
+        }
+    }
+}
